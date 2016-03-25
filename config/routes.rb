84508f57048ebeb9c8f34e937261bfcd0a3b0c54
Allen Lee <allenwlee130@gmail.com>
2016-03-25 09:32:10 -0700

    add gsdk

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/show' => 'home#show'
+  get 'home/get' => 'home#get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
